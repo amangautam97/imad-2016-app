25fc6845c8800a1ce8a49db8d46b655b6de357ef
amangautam97 <aman.gautam@somaiya.edu>
2016-10-01 23:34:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/goku.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'goku.jpg'));
 });
 
