ad7bef65294ccc99302103492edeaa3e00064d82
amangautam97 <aman.gautam@somaiya.edu>
2016-10-02 00:53:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,12 +8,8 @@ function moveRight() {
     marginLeft = marginLeft + 3;
     img.style.marginLeft = marginLeft + 'px';
 }
-var marginRight=0;
-function moveLeft() {
-    marginRight = marginRight + 3;
-    img.style.marginRight = marginRight + 'px';
-}
+
 img.onclick = function() {
     var interval = setInterval(moveRight, 50);
-    var interval = setInterval(moveLeft, 10000);
+   
 };
