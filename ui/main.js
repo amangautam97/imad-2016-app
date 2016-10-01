74538e1d61aeec49b9dc637c84d88db4e2cdae9d
amangautam97 <aman.gautam@somaiya.edu>
2016-10-02 00:56:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,11 @@ var img = document.getElementById('goku');
 
 var marginLeft=0;
 function moveRight() {
-    marginLeft = marginLeft + 50000;
+    marginLeft = marginLeft + 3;
     img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function() {
-    var interval = setInterval(moveRight, 50000);
+    var interval = setInterval(moveRight, 50);
    
 };
