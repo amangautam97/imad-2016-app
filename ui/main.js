5242659f2f9c3a81c4b11aa8c19304471e4ab5b3
amangautam97 <aman.gautam@somaiya.edu>
2016-10-02 16:37:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,17 @@
 var button = document.getElementById('counter');
-var counter=0;
+
 button.onclick= function() {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-}
+    
+    var request = new XMLHttpRequet();
+    request.onreadystatechange = function (){
+        if (request.readyState === XMLHttpRequest.DONE){
+            if (request.status === 200) {
+                var counter = request.responeText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+   request.open('GET', 'http://amangautam97.imad.hasura-app.io/counter', true);
+   request.send(null);
+};
